7f5019c0cd13ac07ab264f57561b75252fa83f40
wthoutjc <juancamilorr2012@hotmail.com>
2024-07-04 16:29:48 -0500

    ready for deploy

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from fastapi.middleware.cors import CORSMiddleware
 from src.api.v1.endpoints.load import router
 from src.core.config import settings
 import logging
+from mangum import Mangum
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = FastAPI(title="ETL Load Microservice")
+handler = Mangum(app)
 
 app.include_router(router, prefix=settings.API_V1_STR)
 
